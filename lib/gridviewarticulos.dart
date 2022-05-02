d66cfd9063e3de89966db3c2f956e6be9f219104
FlutLab.io <>
2022-05-02 18:11:59 +0000

    Autosave

--- lib/gridviewarticulos.dart
+++ lib/gridviewarticulos.dart
@@ -21,7 +21,30 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Flutter GridView"),
+          title: const Text("My App FARMACIA"),
+          centerTitle: true,
+          backgroundColor: Colors.indigo,
+          automaticallyImplyLeading: true,
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add_box_rounded),
+              onPressed: () {
+                print("Soy lupita");
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_business),
+              onPressed: () {
+                print("Soy lupita");
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {},
+            )
+          ],
+          elevation: 8,
+          shadowColor: Colors.blue,
         ),
         body: GridView.custom(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
